00cf42abef0ce4f45a15bd9d16d96a74c020a2d3
zdravki4 <zdravko.gospodinov@gmail.com>
2019-03-19 18:28:07 +0200

    Main CRUD methods extracted in module. Export DB as well

--- IDbManager.js
+++ IDbManager.js
@@ -1,134 +1,141 @@
-var IDbManager = (function (){
-    const dbName = "ProjectNotesDB";
-    const storeName = "project-notes";
+var IDbManager = (function () {
+  const dbName = 'ProjectNotesDB'
+  const storeName = 'project-notes'
+  window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
+  window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction
+  window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+  var db, data, proj
+  // Main functionality
+  if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+  } else {
+    var request = window.indexedDB.open(dbName, 3)
+    request.onupgradeneeded = function (event) {
+      db = event.target.result
+      // console.log(db)
+      if (!db.objectStoreNames.contains(storeName)) {
+        var os = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true })
+      // console.log(os)
+      }
+    }
+    request.onsuccess = function (event) {
+      // console.log('sucsess')
+      db = event.target.result
+    // console.log(db)
+    // showContent()
+    }
+    request.onerror = function (event) {
+      // DO something
+    }
+  }
+  return {
+    objectDataAdded: false,
+    showContent: function () {
+      console.log(data)
+    },
+    getData: function () {
+      var tr = db.transaction([storeName], 'readonly')
+      var store = tr.objectStore(storeName)
+      //   console.log(store)
+      var requestAll = store.getAll()
+      //   console.log(requestAll)
 
-    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-    window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-    window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+      requestAll.onsuccess = function (event) {
+        data = event.target.result
+      }
+    },
+    exportData: function () {
+      //   this.getData()
+      var tr = db.transaction([storeName], 'readonly')
+      var store = tr.objectStore(storeName)
 
-    var db, data, proj;
-    
-    // Main functionality
-    if (!window.indexedDB) {
-        window.alert("Your browser doesn't support a stable version of IndexedDB.");
-    } else {
-        var request = window.indexedDB.open(dbName, 3);
+      var requestAll = store.getAll()
+      var idbMan = this
+      requestAll.onsuccess = function (event) {
+        var records = event.target.result
+        var dataToString = JSON.stringify(records)
+        var exportFileDbName = 'projects-notes-db'
+        console.log(dataToString)
+        idbMan.download(exportFileDbName, dataToString)
+      }
+    },
+    download: function (filename, text) {
+      var element = document.createElement('a')
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+      element.setAttribute('download', filename)
+      element.style.display = 'none'
+      document.body.appendChild(element)
+      element.click()
+      document.body.removeChild(element)
+    },
+    importDb: function (file) {
+      // console.log(file)
 
-        request.onupgradeneeded = function (event) {
-            db = event.target.result;
-            // console.log(db);
-            if (!db.objectStoreNames.contains(storeName)) {
-                var os = db.createObjectStore(storeName, { keyPath: "id", autoIncrement: true });
-                console.log(os);
-            }
-        };
-        request.onsuccess = function (event) {
-            // console.log('sucsess');
-            db = event.target.result;
-            // console.log(db);
-            // showContent();
+      var reader = new FileReader()
+      reader.onload = function (e) {
+        var content = e.target.result
+        var importedData = JSON.parse(content)
+
+        var tr = db.transaction([storeName], 'readwrite')
+        var store = tr.objectStore(storeName)
+        // console.log(store)
+        for (let index = 0; index < importedData.length; index++) {
+          const element = importedData[index]
+          store.add(element)
         }
-        request.onerror = function (event) {
-            //DO something;
-        };
-    };
+      }
+      reader.readAsText(file)
+    },
+    addProject: function (dataObject) {
+      var tr = db.transaction([storeName], 'readwrite')
+      var store = tr.objectStore(storeName)
 
-    return {
-        showContent: function () {
-            console.log(data);
-        },
-        getData: function () {
-            var tr = db.transaction([storeName], "readonly");
-            var store = tr.objectStore(storeName);
-            console.log(store);
-            var requestAll = store.getAll();
-            console.log(requestAll);
-            // var d;
-            requestAll.onsuccess = function (event) {
-                data = event.target.result;
-            }
-        },
-        exportData: function () {
-            var dataToString = JSON.stringify(data);
-            var exportFileDbName = 'projects-notes-db';
-            download(exportFileDbName, dataToString);
-            // console.log(dataToString);
-        },
-        download: function (filename, text) {
-            var element = document.createElement('a');
-            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-            element.setAttribute('download', filename);
-            element.style.display = 'none';
-            document.body.appendChild(element);
-            element.click();
-            document.body.removeChild(element);
-        },
-        importDb: function (evt) {
-            var x = document.getElementById("input-file");
-            if ('files' in x) {
-                var file = x.files[0];
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    var content = e.target.result;
-                    var importedData = JSON.parse(content);
-                    var tr = db.transaction([storeName], "readwrite");
-                    var store = tr.objectStore(storeName);
-                    for (let index = 0; index < importedData.length; index++) {
-                        const element = importedData[index];
-                        var req = store.add(element);
-                    }
-                }
-                reader.readAsText(file)
-            }
-        },
-        addProject: function (dataObject) {
-            // console.log("accessed");
-            var tr = db.transaction([storeName], "readwrite");
-            var store = tr.objectStore(storeName);
-            
-            //Adding
-            var req = store.add(dataObject);
-            // console.log(req);
-            req.onsuccess = () => {
-                console.log('project created');
-                // getData();
-            };
-            req.onerror = () => { console.log("something went wrong") };
-        },
-        getProject:function () {
-            var tr = db.transaction([storeName], "readwrite");
-            var store = tr.objectStore(storeName);
-            var projectGetRequest = store.get(2);
-            // console.log(projectToEdit);
-            projectGetRequest.onsuccess = function (e) {
-                proj = e.target.result;
-                console.log(proj);
-            }
-        },
-        editProject: function (editedData) {
-            // var tr = db.transaction([storeName], "readwrite");
-            // var store = tr.objectStore(storeName);
-            // // console.log(proj.id);
-            // // proj.client = "VM Petroleum";
-    
-            // store.put(editedData);
-        },
-        deleteProject:function (id) {
-            var tr = db.transaction([storeName], "readwrite");
-            var store = tr.objectStore(storeName);
+      var req = store.add(dataObject)
+      // console.log(req)
+      req.onsuccess = () => {
+        console.log('project created')
+      }
+      req.onerror = () => {
+        console.log('something went wrong')}
+      objectDataAdded = true
+    },
+    getProject: function () {
+      var tr = db.transaction([storeName], 'readwrite')
+      var store = tr.objectStore(storeName)
+      var projectGetRequest = store.get(2)
+      // console.log(projectToEdit)
+      projectGetRequest.onsuccess = function (e) {
+        proj = e.target.result
+        console.log(proj)
+      }
+    },
+    editProject: function (editedData) {
+      var tr = db.transaction([storeName], 'readwrite')
+      var store = tr.objectStore(storeName)
 
-            store.delete(2);
-            
-        },
-        clearDb:function () {
-            console.log('deleting');
-            // console.log(db);
-            console.log(window.indexedDB);
-            var reqDelete = window.indexedDB.deleteDatabase(dbName);
-            location.reload();
+      store.put(editedData)
+    },
+    deleteProject: function (id) {
+      var tr = db.transaction([storeName], 'readwrite')
+      var store = tr.objectStore(storeName)
+      var reqKeyes = store.getAllKeys()
+      reqKeyes.onsuccess = function (e) {
+        if (e.target.result.includes(id)) {
+          var reqDelete = store.delete(id)
+          reqDelete.onsuccess = function () {
+            console.log('object deleted')
+          }
+        } else {
+          console.log('Invalid id')
         }
-    };
-
-    
-})();
-
+      }
+    },
+    clearDb: function () {
+      console.log('deleting')
+      // console.log(db)
+      console.log(window.indexedDB)
+      var reqDelete = window.indexedDB.deleteDatabase(dbName)
+      location.reload()
+    }
+  }
+})()
